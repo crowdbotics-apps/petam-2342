b15ed04a06e815451ed365372fa245f484d7b293
Crowdbotics <team@crowdbotics.com>
2019-04-17 04:36:19 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.petam_2342.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "heroku" {
+  version = "~> 1.5"
+}
+provider "cloudflare" {
+  version = "~> 1.9"
+}
+terraform {
+  backend "s3" {
+    bucket  = "crowdbotics-terraform"
+    key     = "petam-2342"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
